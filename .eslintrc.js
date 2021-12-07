8d26a3b0e37ea8a6ff58bbe1ad0e6d53442a8119
Philipp <Philipp000@users.noreply.github.com>
2021-12-07 18:23:26 +0100

    GH Pages linting (#69)
    * Add basic pre-commit file to website branch
    
    Use pre-commit, as it generally should be installed through master branch, and will prevent commits, when it can't find the config file (even in a different branch)
    
    * Add and apply eslinting rules to js
    
    * Add ci via github action to the gh-pages website
    
    * Add yaml configuration files to pre-commit
    
    This enforces a new line at the end of the files
    
    * Add info for global variables

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "prefer-const": ["error"],
+        "no-var": ["error"],
+        "no-multiple-empty-lines": ["error"],
+        "keyword-spacing": ["error"]
+    }
+};

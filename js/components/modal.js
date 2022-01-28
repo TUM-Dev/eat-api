f3b9bcb311d359415928985a07336b455a808795
Philipp Wenner <philipp.wenner@web.de>
2022-01-28 19:13:41 +0100

    Move modal close button into the modal

--- js/components/modal.js
+++ js/components/modal.js
@@ -14,13 +14,14 @@ export default function Modal() {
                 m("span", {class: "is-clickable", onclick: () => showModal = true}, vnode.children),
                 m("div", {class: modalClass}, [
                     m("div", {class: "modal-background", onclick: () => showModal = false}),
-                    m("div", {class: "modal-content"},
+                    m("div", {class: "modal-content"}, [
+                        m("button", {class: "modal-close is-large", "aria-label": "close", onclick: () => showModal = false}),
                         m("div", {class: "card"},
                             m("div", {class: "card-content"},
                                 m("div", {class: "content"},
                                     vnode.attrs.content
-                                )))),
-                    m("button", {class: "modal-close is-large", "aria-label": "close", onclick: () => showModal = false})
+                                )))
+                    ]),
                 ])
             ]);
         }

0eaabd57d41888127ef3bd79d11819b7c663b630
Philipp Wenner <philipp.wenner@web.de>
2022-02-02 18:42:37 +0100

    Feature: skip days without a menu on date selection

--- js/components/date-selection.js
+++ js/components/date-selection.js
@@ -3,18 +3,38 @@ import m from "../external/mithril.module.js";
 import {getHref, getUrlDate} from "../modules/url-utils.js";
 import translate from "../modules/translation.js";
 import Tooltip from "./tooltip.js";
+import {getNextAvailableDate} from "../modules/api.js";
+import {getLanguage} from "../modules/translation.js";
 
 
 export default function DateSelection() {
+    let before, after, search;
     return {
+        onupdate: function () {
+            if (search?.valueOf() === getUrlDate().valueOf()) {
+                return;
+            }
+            search = getUrlDate();
+            after = false;
+            before = false;
+
+            const mensa = m.route.param("mensa");
+            const languageObject = getLanguage();
+
+            const startAfter = copyDate(search);
+            startAfter.setDate(startAfter.getDate() + 1);
+            getNextAvailableDate({starting: startAfter, mensa, languageObject})
+                .then(d => after = d)
+                .catch(() => after = false);
+
+            const startBefore = copyDate(search);
+            startBefore.setDate(startBefore.getDate() - 1);
+            getNextAvailableDate({starting: startBefore, mensa, languageObject, step: -1})
+                .then(d => before = d)
+                .catch(() => before = false);
+        },
         view: function () {
             const currentDate = getUrlDate();
-
-            const before = copyDate(currentDate);
-            before.setDate(before.getDate() - 1);
-            const after = copyDate(currentDate);
-            after.setDate(after.getDate() + 1);
-
             const mensa = m.route.param("mensa");
 
             return m("div", {class: "field has-addons"}, [
